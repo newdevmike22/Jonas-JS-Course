8bda86c14eb2ca4cb53e8a39bd2c37faa2221f4b
newdevmike22 <paola@dominicansoul.com>
2026-02-28 18:19:20 -0400

    JavaScript Fundamentals – Part 1 - Values and Variables
    JavaScript Fundamentals – Part 1. Learning values and variables + practice assignment.

--- 01-Fundamentals-Part-1/index.js
+++ 01-Fundamentals-Part-1/index.js
@@ -1,4 +1,20 @@
 let js = "Oh so funky!";
-if (js === "Oh so funky!") alert("I love JavaScript!");
+//if (js === "Oh so funky!") alert("I love JavaScript!");
 
+// values
 console.log(40 + 8 * 5);
+console.log("Mike");
+console.log(56);
+
+// variables
+let firstName = "Susan";
+firstName = "Betty";
+console.log(firstName);
+console.log(`${firstName} is a very beautiful woman.`);
+
+let superHero = "Hulk";
+let color = "green";
+console.log(`The ${superHero} is a mean ${color} smashing machine!`);
+
+let firstJob = "graphic artist";
+console.log(`${firstName} got her first real job as a ${firstJob} out of college.`);
---
+++ 01-Fundamentals-Part-1/practice.js
@@ -0,0 +1,9 @@
+// 1. Values and Variables
+// Declare variables called country, continent and population and assign their values according to your own country (population in millions).
+//Log their values to the console.
+
+let country = "United States";
+let continent = "North Amercica";
+let population = 349;
+
+console.log(`The ${country} is located in ${continent}. It has a population of ${population} million according to the latest data.`);
