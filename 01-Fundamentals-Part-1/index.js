9148e52736f6e857c6ff8921544ff2da765ec66d
newdevmike22 <paola@dominicansoul.com>
2026-02-28 20:03:35 -0400

    JavaScript Fundamentals – Part 1 - JavaScript Fundamentals – Part 1 - JavaScript Fundamentals – Part 1 - Data Types
    JavaScript Fundamentals – Part 1. Learning data types + practice assignment.

--- 01-Fundamentals-Part-1/index.js
+++ 01-Fundamentals-Part-1/index.js
@@ -1,3 +1,4 @@
+/*
 let js = "Oh so funky!";
 //if (js === "Oh so funky!") alert("I love JavaScript!");
 
@@ -18,3 +19,27 @@ console.log(`The ${superHero} is a mean ${color} smashing machine!`);
 
 let firstJob = "graphic artist";
 console.log(`${firstName} got her first real job as a ${firstJob} out of college.`);
+*/
+
+// 7 primative data types
+let age = 16; // number
+let city = "Puerto Plata"; // string
+let fullName = true; // boolean
+let place; // undefined
+// null
+// symbol (ES2015)
+// bigint (ES20)
+let javaScriptIsFun = true;
+console.log(typeof javaScriptIsFun);
+console.log(typeof age);
+console.log(typeof city);
+console.log(city.length);
+
+javaScriptIsFun = "YES!";
+console.log(`Do you like JavaScript? ${javaScriptIsFun}`);
+console.log(place);
+console.log(typeof place);
+
+place = "St. Louis, MO";
+console.log(`I left my heart in ${place}.`);
+console.log(typeof place);
--- 01-Fundamentals-Part-1/practice.js
+++ 01-Fundamentals-Part-1/practice.js
@@ -7,3 +7,17 @@ let continent = "North Amercica";
 let population = 349;
 
 console.log(`The ${country} is located in ${continent}. It has a population of ${population} million according to the latest data.`);
+
+// 2. Data Types
+// Declare a variable called isIsland and set its value according to your country. The variable should hold a Boolean value. Also declare a variable language, but don't assign it any value yet.
+// Log the types of isIsland, population, country and language to the console.
+
+country = "Dominican Republic";
+population = 11.5;
+let isIsland = true;
+let language;
+console.log(`Is the ${country} an island? ${isIsland}`);
+console.log(`The ${country} is a part of ${continent} with a population of ${population} million.`);
+console.log(language);
+language = "Spanish";
+console.log(`${language} is the official language of ${country}.`);
